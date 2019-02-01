2e019396b2e477eb7691a6dc5ca214865e1be13b
Abdulkhakim Abdrakhman <aabdrakh@e1z2r3p39.42.us.org>
2019-02-01 12:01:48 -0800

    let's get it started

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdrakh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/25 17:53:12 by aabdrakh          #+#    #+#             */
+/*   Updated: 2018/09/25 17:53:14 by aabdrakh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+static int		ft_read(int fd, char **str)
+{
+	int			size;
+	char		*s;
+	char		buf[BUFF_SIZE + 1];
+
+	size = read(fd, buf, BUFF_SIZE);
+	if (size == -1)
+		return (-1);
+	buf[size] = '\0';
+	s = *str;
+	*str = ft_strjoin(*str, buf);
+	if (*s != 0)
+		free(s);
+	return (size);
+}
+
+static int		ready(char **str, char **line, char *s)
+{
+	int		flag;
+	char	*new;
+
+	flag = 0;
+	if (*s == '\n')
+		flag = 1;
+	*s = 0;
+	*line = ft_strjoin("", *str);
+	if (!flag)
+	{
+		if (!(ft_strlen(*str)))
+			return (0);
+		else
+		{
+			*str = "\0";
+			return (1);
+		}
+	}
+	new = *str;
+	*str = ft_strjoin(s + 1, "");
+	free(new);
+	return (flag);
+}
+
+int				get_next_line(const int fd, char **line)
+{
+	int			size;
+	char		*s;
+	static char	*str[MAX_FD];
+
+	if (!line || fd < 0 || BUFF_SIZE < 1)
+		return (-1);
+	if (!str[fd])
+		str[fd] = "";
+	size = BUFF_SIZE;
+	while (line)
+	{
+		s = str[fd];
+		while (*s || size < BUFF_SIZE)
+		{
+			if (*s == '\n' || !(*s))
+				return (ready(&str[fd], line, s));
+			s++;
+		}
+		size = ft_read(fd, &str[fd]);
+		if (size == -1)
+			return (-1);
+	}
+	return (1);
+}
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		ft_putendl(line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
+}
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabdrakh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/09/25 17:53:20 by aabdrakh          #+#    #+#             */
+/*   Updated: 2018/09/25 17:53:23 by aabdrakh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include "../libft/libft.h"
+# include <unistd.h>
+# include <stdlib.h>
+
+# define BUFF_SIZE 32
+# define DESC 10240
+# define MAX_FD 4864
+
+int get_next_line(const int fd, char **line);
+
+#endif
